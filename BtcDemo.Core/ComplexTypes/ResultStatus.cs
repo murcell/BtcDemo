abca569c3a78268f21c66935cba07a962d99e945
= <=>
2023-09-15 15:48:00 +0300

    Add project files.

---
+++ BtcDemo.Core/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace BtcDemo.Core.ComplexTypes
+{
+	public enum ResultStatus
+	{
+		Success = 0,
+		Error = 1,
+		Warning = 2,
+		Info = 3,
+
+		Forbidden = 403,
+		UnAuthorized = 401,
+		ServerError = 500
+
+	}
+}
